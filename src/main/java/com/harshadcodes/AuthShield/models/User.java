d79c5bde8a0e6de0f87814d7184d35ef2279fe8e
Harshad Yewale <harshadyewale73@gmail.com>
2026-02-14 20:59:17 +0530

    done with the user entity and repository

--- src/main/java/com/harshadcodes/AuthShield/models/User.java
+++ src/main/java/com/harshadcodes/AuthShield/models/User.java
@@ -1,9 +1,33 @@
 package com.harshadcodes.AuthShield.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    private String userId;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
     private String verifyOtp;
@@ -11,4 +35,11 @@ public class User {
     private Long verifyOtpExpireAt;
     private String resetOtp;
     private Long resetOtpExpiredAt;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
---
+++ src/main/java/com/harshadcodes/AuthShield/repositories/userRepository.java
@@ -0,0 +1,14 @@
+package com.harshadcodes.AuthShield.repositories;
+
+import com.harshadcodes.AuthShield.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface userRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
